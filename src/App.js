b953fbacd766be589ef95b0f12a14e90389709e4
AdithaBuwaneka <adithabuwaneka0@gmail.com>
2024-09-24 03:48:06 +0530

    new chnages

--- src/App.js
+++ src/App.js
@@ -99,7 +99,7 @@ function App() {
               <select
                 value={sourceCurrency} // Set the selected value
                 onChange={(e) => setsourceCurrency(e.target.value)}
-                className=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500"
+                className=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-700 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500"
                 name="sourceCurrency"
                 id="sourceCurrency"
               >
